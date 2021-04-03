0ada656c691ec0c9d7b0bb22bb81655775c61d58
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-03 14:46:03 +0100

    Initial commit

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -o flatcar-install https://raw.githubusercontent.com/flatcar-linux/init/flatcar-master/bin/flatcar-install
+curl -o ignition.json https://raw.githubusercontent.com/alanjjenkins/home-k8s-init/master/ignition.json
