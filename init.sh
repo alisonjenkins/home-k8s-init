5b90bb6677bec772d09e77d957f36e966fd2b03e
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-03 14:53:18 +0100

    Automatically chmod +x flatcar-install

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -o flatcar-install https://raw.githubusercontent.com/flatcar-linux/init/flatcar-master/bin/flatcar-install
 curl -o ignition.json https://raw.githubusercontent.com/alanjjenkins/home-k8s-init/master/ignition.json
+chmod +x flatcar-install
