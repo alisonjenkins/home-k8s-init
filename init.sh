223e8c448a152da1fc9ab177319f3e95bd9d0841
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-03 15:06:49 +0100

    Add command example to script output

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@
 curl -o flatcar-install https://raw.githubusercontent.com/flatcar-linux/init/flatcar-master/bin/flatcar-install
 curl -o ignition.json https://raw.githubusercontent.com/alanjjenkins/home-k8s-init/master/ignition.json
 chmod +x flatcar-install
+
+echo "Now run something like..."
+echo -e "\t./flatcar-install -d /dev/<disk> -i ignition.json"
